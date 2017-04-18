8a0e957e15a8466f1369de0ff3855392127a3e3a
Nobikachu <nobutaka.sato.ns@gmail.com>
2017-04-18 12:43:15 +1200

    Update Pet.java
    I have added some stats for each pet :).

--- Pet.java
+++ Pet.java
@@ -1,15 +1,33 @@
 
-
 public class Pet extends GameEnvironment {
 	private String petName;
 	private int actionsRemaning = 0;
-	
-	public Pet(String name){
+	private int hunger;
+	private int sleep;
+	private int aggressive;
+	private int happiness;
+	public Pet(String name, int hungerDrop, int sleepDrop, int durabilityDrop, int happinessDrop){
 		petName = name;
+		hunger = hungerDrop;
+		sleep = sleepDrop;
+		aggressive = durabilityDrop;
+		happiness = happinessDrop;
 	}
 	
 	public String getName(){
 		return petName;
+	}
+	public int getHunger(){
+		return hunger;
+	}
+	public int getSleep(){
+		return sleep;
+	}
+	public int getAgressive(){
+		return aggressive;
+	}
+	public int getHappiness(){
+		return happiness;
 	}
 	
 	public int getActionsRemaning() {
